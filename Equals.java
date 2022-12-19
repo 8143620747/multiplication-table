fe29004799b4108a53f51bd778904295b117288d
G V L KARUNAKAR <121012982+8143620747@users.noreply.github.com>
2022-12-19 14:49:49 +0530

    Add files via upload

---
+++ Equals.java
@@ -0,0 +1,13 @@
+class Equals
+{
+public static void main(String[]args)
+{
+String s=new String("hellojava");
+String s1=s;
+if(s.equals(s1))
+System.out.println("both are equals");
+else
+
+System.out.println("both are not eauals");
+}
+}
